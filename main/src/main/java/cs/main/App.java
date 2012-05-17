d26f25baa4b3c3a85e41bcbd28cf68a0a8b11921
Luis Lezcano Airaldi <luislezcair@gmail.com>
2012-05-17 17:45:38 -0300

    Nuevo proyecto

---
+++ main/src/main/java/cs/main/App.java
@@ -0,0 +1,13 @@
+package cs.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
